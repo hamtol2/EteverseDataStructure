479e72e8eac1b713fbfd8d034b9c2b543ada4ff0
RonnieJ <ronniej@naver.com>
2024-03-21 10:18:41 +0900

    해시 테이블 코드 완성 및 데모 코드 업로드

--- 8.HashTable/HashTable.cs
+++ 8.HashTable/HashTable.cs
@@ -70,12 +70,98 @@ public class HashTable<Tkey, TValue>
     public void Delete(Tkey key)
     {
         // 1. 키를 기반으로 저장되어 있는 연결리스트의 인덱스를 찾는다.
+        // - key를 기반으로 해시를 구한 뒤 나머지 연산을 통해 인덱스를 구한다.
+        int bucketIndex = GenerateKey(key) % bucketCount;
+
+        // 데이터를 추가할 연결리스트 참조 값 저장 (임시로, 편의를 위해).
+        //LinkedList<Pair<Tkey, TValue>> bucketList = table[bucketIndex];
+        var bucketList = table[bucketIndex];
 
         // 2. 1에서 찾은 연결리스트에서 동일한 키를 가진 데이터가 있는지 찾는다.
+        for (int ix = 0; ix < bucketList.Count; ++ix)
+        {
+            // 동일한 키가 존재하는 지 확인.
+            if (bucketList[ix].data.key.Equals(key) == true)
+            {
+                // 연결 리스트에서 해당 데이터를 삭제.
+                bucketList.Delete(bucketList[ix].data);
+                Console.WriteLine($"키 {key} 항목이 삭제되었습니다.");
+                return;
+            }
+        }
 
-        // 3. 있으면 삭제, 없으면 삭제 실패.
+        // 3. 없으면 삭제 실패.
+        Console.WriteLine("Error: 삭제할 데이터를 찾지 못했습니다.");
     }
-    
+
+    // 검색 함수1 -> Value 값만 반환.
+    public bool Find(Tkey key, ref TValue outValue)
+    {
+        // 키를 기반으로 인덱스 찾기.
+        int bucketIndex = GenerateKey(key) % bucketCount;
+
+        // 검색한 인덱스로 연결 리스트 참조 저장 (편의를 위해).
+        var bucketList = table[bucketIndex];
+
+        // 연결 리스트 안에서 키를 가진 데이터를 검색 후 검색이 되면 해당 데이터 반환. (검색 성공).
+        for (int ix = 0; ix < bucketList.Count; ++ix)
+        {
+            // 키를 가진 데이터가 있으면, outValue에 해당 값을 할당하고, 함수 종료.
+            if (bucketList[ix].data.key.Equals(key) == true)
+            {
+                outValue = bucketList[ix].data.value;
+                return true;
+            }
+        }
+
+        // 위에서 검색에 실패했으면, 데이터 찾기 실패.
+        return false;
+    }
+
+    // 검색 함수2 -> Pair<Key, Value> 쌍을 반환.
+    public bool Find(Tkey key, ref Pair<Tkey, TValue> outPair)
+    {
+        // 키를 기반으로 인덱스 찾기.
+        int bucketIndex = GenerateKey(key) % bucketCount;
+
+        // 검색한 인덱스로 연결 리스트 참조 저장 (편의를 위해).
+        var bucketList = table[bucketIndex];
+
+        // 연결 리스트 안에서 키를 가진 데이터를 검색 후 검색이 되면 해당 데이터 반환. (검색 성공).
+        for (int ix = 0; ix < bucketList.Count; ++ix)
+        {
+            // 키를 가진 데이터가 있으면, outValue에 해당 값을 할당하고, 함수 종료.
+            if (bucketList[ix].data.key.Equals(key) == true)
+            {
+                outPair = bucketList[ix].data;
+                return true;
+            }
+        }
+
+        // 위에서 검색에 실패했으면, 데이터 찾기 실패.
+        return false;
+    }
+
+    // 출력 함수.
+    public void Print()
+    {
+        // 이중 루프.
+        for (int ix = 0; ix < bucketCount; ++ix)
+        {
+            // 예외 처리 - 연결 리스트가 비었으면 점프.
+            if (table[ix].IsEmpty == true)
+            {
+                continue;
+            }
+
+            // 연결 리스트 순회하면서 데이터 출력.
+            for (int jx = 0; jx < table[ix].Count; ++jx)
+            {
+                Console.WriteLine($"키: { table[ix][jx].data.key } 값: { table[ix][jx].data.value }");
+            }
+        }
+    }
+
     // 해시 생성 함수.
     // 키를 전달 받아서, 문자열로 변환한 뒤, 이를 활용해서 해시 값을 구함.
     private int GenerateKey(Tkey key)
--- 8.HashTable/Program.cs
+++ 8.HashTable/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace _8.HashTable
+﻿using System;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // 해시 테이블 생성.
+        HashTable<string, string> table = new HashTable<string, string>();
+
+        // 자료 추가.
+        table.Add("Ronnie", "010-12345678");
+        table.Add("Ronnie", "010-29873498");
+        table.Add("Kevin", "010-41987344");
+        table.Add("Baker", "010-31284973");
+        table.Add("Taejun", "010-49328574");
+
+        // 출력.
+        table.Print();
+        Console.WriteLine();
+
+        // 검색.
+        Pair<string, string> outPair = new Pair<string, string>();
+        if (table.Find("Ronnie", ref outPair) == true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"검색 결과1: {outPair.key}, {outPair.value} ");
         }
+
+        if (table.Find("Test", ref outPair) == true)
+        {
+            Console.WriteLine($"검색 결과2: {outPair.key}, {outPair.value} ");
+        }
+
+        // 삭제.
+        table.Delete("Ronnie");
+        table.Delete("Baker");
+        table.Delete("Test");
+
+        // 최종 출력.
+        Console.WriteLine("\n최종 출력.");
+        table.Print();
+        
+        // 종료 대기.
+        Console.ReadKey();
     }
 }
