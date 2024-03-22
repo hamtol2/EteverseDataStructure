12bd59cbd0dd8abe09d2c9f846d1e6758cbe0f1c
RonnieJ <ronniej@naver.com>
2024-03-22 15:50:24 +0900

    이진 탐색 트리 클래스 구현 추가

--- 11.BinarySearchTree/BinarySearchTree.cs
+++ 11.BinarySearchTree/BinarySearchTree.cs
@@ -41,12 +41,129 @@ public class BinarySearchTree
         }
 
         // Todo: 2와 3의 경우를 위해 재귀 함수 작성 후 호출해야 함.
+        Root = InsertNodeRecursive(Root, null, newData);
+        return true;
+    }
 
-        // Todo: 코드 제대로 다시 작성 필요.
-        return false;
-    } 
+    // 재귀적으로 삽입을 처리하는 함수.
+    // node: 현재 비교하는 노드.
+    // parent: node의 부모 노드.
+    // newData: 삽입하려는 새 데이터.
+    private Node InsertNodeRecursive(Node node, Node parent, int newData)
+    {
+        // node가 null이면 새로 생성 후 생성한 노드를 반환.
+        if (node == null)
+        {
+            // 새 노드 생성.
+            Node newNode = new Node(newData);
+
+            // 새 노드의 부모 노드 지정.
+            newNode.Parent = parent;
+
+            // 노드 반환.
+            return newNode;
+        }
+
+        // 추가하려는 값이 비교 노드보다 작으면, 왼쪽 서브 트리로 탐색.
+        if (node.Data > newData)
+        {
+            node.Left = InsertNodeRecursive(node.Left, node, newData);
+        }
+
+        // 추가하려는 값이 비교 노드보다 크면, 오른쪽 서브 트리로 탐색.
+        else
+        {
+            node.Right = InsertNodeRecursive(node.Right, node, newData);
+        }
+
+        return node;
+    }
 
     // 삭제.
+    public void DeleteNode(int deleteData)
+    {
+        // 재귀적으로 삭제하는 함수를 호출.
+        // 삭제 연산의 시작은 Root 노드.
+        // 예외 처리가 안된 코드.
+        Root = DeleteNodeRecursive(Root, deleteData);
+    }
+
+    // 삭제 재귀 함수.
+    private Node DeleteNodeRecursive(Node node, int deleteData)
+    {
+        // 종료 조건.
+        // 노드가 null이면 삭제할 노드를 찾지 못한 것이기 때문에 null을 반환하고 종료.
+        if (node == null)
+        {
+            return null;
+        }
+
+        // 삭제하려는 데이터가 비교 데이터보다 작으면 왼쪽 서브 트리로 이동.
+        if (node.Data > deleteData)
+        {
+            node.Left = DeleteNodeRecursive(node.Left, deleteData);
+        }
+
+        // 삭제하려는 데이터가 비교 데이터보다 크면 오른쪽 서브 트리로 이동.
+        else if (node.Data < deleteData)
+        {
+            node.Right = DeleteNodeRecursive(node.Right, deleteData);
+        }
+
+        // 찾으면, 삭제 처리.
+        else
+        {
+            // 경우1 - 자식이 없는 경우. (둘 모두 null인 경우)
+            if (node.Left == null && node.Right == null)
+            {
+                return null;
+            }
+
+            // 경우3 - 자식이 둘 다 있는 경우. (둘 모두 null이 아닌 경우).
+            if (node.Left != null && node.Right != null)
+            {
+                // 삭제되는 노드의 오른쪽 서브 트리에서 가장 작은 값을 검색해서 삭제하는 노드의 값을 업데이트.
+                node.Data = SearchMinValue(node.Right).Data;
+
+                // 위에서 구한 오른쪽 서브 트리에서 가장 작은 값을 가진 노드를 삭제.
+                node.Right = DeleteNodeRecursive(node.Right, node.Data);
+            }
+
+            // 경우2 - 자식이 하나만 있는 경우 (둘 중 하나만 null인 경우).
+            if (node.Left == null)
+            {
+                return node.Right;
+            }
+
+            else if (node.Right == null)
+            {
+                return node.Left;
+            }
+        }
+
+        return node;
+    }
+
+    // 최소값 검색 함수.
+    // node: 검색을 시작하는 노드.
+    private Node SearchMinValue(Node node)
+    {
+        // 검색 시작.
+        //int minValue = node.Data;
+
+        // 왼쪽 서브 트리로 계속해서 이동하면서, 최소 값 업데이트.
+        while (node.Left != null)
+        {
+            // 왼쪽 노드의 값을 최소 값으로 업데이트.
+            //minValue = node.Left.Data;
+
+            // 왼쪽 서브 노드로 이동.
+            node = node.Left;
+        }
+
+        // 최소 값을 가진 노드를 반환.
+        return node;
+    }
 
     // 검색.
     public bool SearchNode(int data)
@@ -82,5 +199,27 @@ public class BinarySearchTree
         }
     }
 
-    // 출력.
+    // 출력 (순회).
+    public void InorderTraverse()
+    {
+        // 출력을 위해 중위 순회 방식으로 재귀 함수를 호출.
+        // 탐색 시작은 루트 노트에서 부터.
+        InorderTraverseRecursive(Root);
+    }
+
+    // 중위 순회 재귀 함수.
+    private void InorderTraverseRecursive(Node node)
+    {
+        if (node != null)
+        {
+            // 왼쪽 서브 트리를 중위 순회.
+            InorderTraverseRecursive(node.Left);
+
+            // 부모 노드 처리.
+            Console.WriteLine($"{node.Data} ");
+
+            // 오른쪽 서브 트리를 중위 순회.
+            InorderTraverseRecursive(node.Right);
+        }
+    }
 }
